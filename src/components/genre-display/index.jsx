db870926e637b6ab93ce771ec1fc1ea33deafb8b
raieegulhane <gulhaneraiee17@gmail.com>
2022-09-07 22:02:07 +0530

    genre-display filters

--- src/components/genre-display/index.jsx
+++ src/components/genre-display/index.jsx
@@ -1,44 +1,65 @@
 import "./genre-display.css";
-import { NavLink } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { genreDisplayList } from "./genre-list";
+import { useFilter } from "../../contexts";
 
 export const GenreDisplay = () => {
-    
+    const navigate = useNavigate();
+    const { filterDispatch } = useFilter();
+
+    const genreOnClickHandler = (event, filterAction) => {
+        event.preventDefault();
+
+        filterDispatch({ type: filterAction });
+        navigate("/bookstore");
+    }
+
     return(
         <div className="genre-wrapper flex-col flex_align-middle">
             <h1 className="genre-heading">Genre</h1>
 
             <section className="genre-container flex-row flex_justify-sa flex_align-middle">
                 {
-                    genreDisplayList.map(({ _id, genreName, genreImg, alt }) => {
+                    genreDisplayList.map(({ 
+                        _id, 
+                        genreName, 
+                        genreImg, 
+                        alt, 
+                        actionType 
+                    }) => {
                         return(
                             <div 
                                 key={_id}
                                 className="flex-col flex_align-middle"
                             >
-                                <img 
-                                    className="genre-img img-100pr img-rd"
-                                    src={genreImg} 
-                                    alt={alt}
-                                />
                                 <button 
-                                    className="genre-name btn-link"
+                                    className="home-genre-btn"
+                                    onClick={(e) => genreOnClickHandler(e, actionType)}
+                                >
+                                    <img 
+                                        className="genre-img img-100pr img-rd"
+                                        src={genreImg} 
+                                        alt={alt}
+                                    />
+                                </button>
+                                <h4 
+                                    className="genre-name"
                                 >
                                     {genreName}
-                                </button>
+                                </h4>
                             </div>
                         );
                     })
                 }
             </section>
 
-            <NavLink 
+            <Link 
                 to="/bookstore"
                 className="genre-btn btn btn-wt-icon btn-outline btn-sq link-noDecoration"
             >
                 <span>Discover All Categories</span>
                 <i className="fa-solid fa-angles-right"></i>
-            </NavLink>
+            </Link>
         </div>
     );
 }
